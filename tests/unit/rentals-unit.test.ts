9574aa6db90718aef2ef77de0816e085a40ea79f
gabriela <gabrielamontanhini@gmail.com>
2023-10-05 17:42:13 -0300

    Feat: testes unitarios

--- tests/unit/rentals-unit.test.ts
+++ tests/unit/rentals-unit.test.ts
@@ -1,5 +1,32 @@
+
+
 describe("Rentals Service Unit Tests", () => {
-  it("should pass", () => {
-    expect(true).toBe(true);
+  it("should create a rental", async() => {
+
+   //TODO
+   //FIXME
+      
+    expect(2).toBe(2);
   })
+
+  it("should finish a rental", async() => {
+   //TODO
+   //FIXME
+    
+    expect(3).toBe(3);
+  })
+  it("shouldn't create a rental for underage if adult film", async() => {
+
+    //TODO
+    //FIXME
+       
+     expect(4).toBe(4);
+   })
+ 
+   it("should get all rentals", async() => {
+    //TODO
+    //FIXME
+     
+     expect(5).toBe(5);
+   })
 })
